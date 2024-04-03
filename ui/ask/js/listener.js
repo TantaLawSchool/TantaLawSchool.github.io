34bae12e97def1622d6ce67ed90aeed703465e25
sulfur 16 <142856282+sulfur-16@users.noreply.github.com>
2024-04-03 20:39:40 +0200

    Add files via upload

---
+++ ui/ask/js/listener.js
@@ -0,0 +1,34 @@
+
+
+
+
+function askButtonListener(e){
+   
+
+  if($("ask_content").value.length==0 || $("ask_content").value.replaceAll(" ","").length==0){
+
+  }else{
+
+    if(isAsk($("ask_content").value)){
+
+
+
+    }else{
+        setAsk($("ask_content").value);
+    }
+
+
+  $("container").innerHTML = `
+  <div class="received"> <img src="../ue/a/sent.png" height="60px"></img> تم ارسال طلبك بنجاح  </div>
+  `;
+
+  }
+
+
+}
+
+
+
+
+
+
