24fa117feb44377e375e4e469f945d288d0a79b3
sulfur 16 <142856282+sulfur-16@users.noreply.github.com>
2024-07-17 03:22:34 +0300

    Add files via upload

--- ui/title/js/environ.js
+++ ui/title/js/environ.js
@@ -1,6 +1,6 @@
 
 var okjscode; /*messagebox.js*/
 var mplayer_packageName = "com.elephant.player";
-var mplayer_update_url = "https://sulfur-16.github.io/mplayer.apk";
-var mplayer_versioncode = 1;
+var mplayer_update_url = "https://sulfur-16.github.io/BitPlayer-v2.apk";
+var mplayer_versioncode = 2;
 
