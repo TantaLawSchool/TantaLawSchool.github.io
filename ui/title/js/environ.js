ae3e626c15c3cb4f243baa7fc210488cff8ecc9f
sulfur 16 <142856282+sulfur-16@users.noreply.github.com>
2024-08-22 15:48:36 +0300

    Delete ui directory

--- ui/title/js/environ.js
+++
@@ -1,6 +0,0 @@
-
-var okjscode; /*messagebox.js*/
-var mplayer_packageName = "com.elephant.player";
-var mplayer_update_url = "https://sulfur-16.github.io/BitPlayer-v2.apk";
-var mplayer_versioncode = 2;
-
